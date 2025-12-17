b106acd85641aa3127d431331235493ef21f26e6
PIAL MOLLAH <pial.mollah.0401@gmail.com>
2025-12-17 21:42:32 +0600

    Implements jinja base template

--- ProjectFour/app.py
+++ ProjectFour/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import time
 
 app = Flask(__name__)
@@ -30,11 +30,32 @@ def addUser():
     data = "User added successfully."
     return render_template("adduser.html", response=data), 200
 
+@app.route("/plain")
+def plainText():
+    return render_template("adduser.html", response="Server Response")
+
+@app.route("/json")
+def json():
+    data = [1, 2, 3, 4]
+    return jsonify(data), 200
+
+
 @app.route("/userlist")
 def userList():
-    return render_template("userlist.html", data = users), 200
-    
+    return render_template("userlist.html", data=users), 200
 
+
+@app.route("/addbyparam")
+def addByParam():
+    username = request.args.get("username")
+    if not username:
+        return "Please enter user name through params."
+    if username in users:
+        return "Name already existed! try another..."
+
+    users.append(username)
+    data = "User added successfully."
+    return render_template("adduser.html", response=data), 200
 
 
 if __name__ == "__main__":
