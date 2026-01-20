6d21c525e9fd4a279155ef7125dc13b121b49bc0
PIAL MOLLAH <pial.mollah.0401@gmail.com>
2026-01-20 17:13:07 +0600

    Connect postgresql with flask and query all data

--- Practice/app.py
+++ Practice/app.py
@@ -1,10 +1,57 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql://postgres:1234@localhost:5432/postgres"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+# Data mode class ------------------------
+class User(db.Model):
+    __tablename__ = "students"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String)
+    department = db.Column(db.String)
+
+
+# ----------------------------------------
+
+
 @app.route("/")
 def home():
-    return "Home Screen"
+    return "<h1>Home Screen</h1>"
+
+
+@app.route("/contact")
+def contact():
+    return "This is contact page!"
+
+
+@app.route("/users")
+def users():
+    users = User.query.all()
+    result = []
+
+    for data in users:
+        result.append(
+            {
+                "id": data.id,
+                "name": data.name,
+                "age": data.age,
+                "gender": data.gender,
+                "department": data.department
+            }
+        )
+
+    return jsonify(result)
 
 
 if __name__ == "__main__":
