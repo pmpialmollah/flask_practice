8ea1663ebec3db5dc549ca2c67bc76724039559c
Pial Mollah <pmpialmollah@email.com>
2026-06-04 20:50:25 +0600

    Modified TestProject/main.py

--- TestProject/main.py
+++ TestProject/main.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def pageNotFound(e):
+    return "Not page found brother."
+
 @app.route("/")
 def index():
     return  "Flask server is running..."
+
+@app.route("/about")
+def about():
+    return "This is about page."
 
 
 if __name__ == "__main__":
